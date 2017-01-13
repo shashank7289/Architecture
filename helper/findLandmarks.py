826676bead1893cf5f6a105e68eecebf6bf847f6
Shashank Dhariwal <shashank.dhariwal@continental-corporation.com>
2017-01-13 18:11:49 +0530

    compatible for frontalisation

--- helper/findLandmarks.py
+++ helper/findLandmarks.py
@@ -9,6 +9,7 @@ from cv2 import circle
 
 class face(object):
     def __init__(self):
+        self.lMarksFloat = self.lMarksInt = []
         self.outline = self.eyeBrows = self.nose = self.leftEye = self.rightEye = self.mouth = []
         self.leftEyeCorner = self.rightEyeCorner = 0
         
@@ -18,6 +19,7 @@ faceObj = face();
 def findLandmarks(img,face,predictor):
     
     lMarks = zeros((68, 2))
+#     lMarks = []
     for (x,y,w,h) in face:
         d = rectangle(x,y,x+w,y+h)
         i = 0
@@ -25,15 +27,16 @@ def findLandmarks(img,face,predictor):
             lMarks = insert(lMarks, i, [p.x, p.y], 0)
             i = i+1
 #             circle(img,(p.x,p.y),1,(0,255,0), 1)
-    lMarks = lMarks.astype(int)
-    faceParts(lMarks)
+    faceObj.lMarksInt = lMarks[:68].astype(int)
+    faceObj.lMarksFloat = lMarks[:68].astype(float)
+    faceParts(faceObj.lMarksInt,faceObj.lMarksFloat)
 
-def faceParts(lMarks):
-    faceObj.outline = array(lMarks[:17])
-    faceObj.eyeBrows = array(lMarks[17:27])
-    faceObj.nose = array(lMarks[27:36])
-    faceObj.leftEye = array(lMarks[36:42])
-    faceObj.rightEye = array(lMarks[42:48])
-    faceObj.leftEyeCorner = array(lMarks[36])
-    faceObj.rightEyeCorner = array(lMarks[45])
-    faceObj.mouth = array(lMarks[48:])
+def faceParts(lMarksInt,lMarksFloat):
+    faceObj.outline = array(lMarksFloat[:17])
+    faceObj.eyeBrows = array(lMarksFloat[17:27])
+    faceObj.nose = array(lMarksFloat[27:36])
+    faceObj.leftEye = array(lMarksFloat[36:42])
+    faceObj.rightEye = array(lMarksFloat[42:48])
+    faceObj.leftEyeCorner = array(lMarksInt[36])
+    faceObj.rightEyeCorner = array(lMarksInt[45])
+    faceObj.mouth = array(lMarksFloat[48:])
